ee4d2738b41e708f92f1138ecceb371568994ad3
Shubham Patil <shubhampatil.ani@gmail.com>
2023-08-17 22:59:58 +0530

    navbar added

---
+++ start/src/Description.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+import image from "./robot.jpg"
+import classes from "./Description.module.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/esm/Button";
+
+const Description = () => {
+      return (
+    <Fragment className={classes.body}>
+        <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Robot character</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <div className={classes.head}>
+        <h1>
+        Let's get start assembling the robot <Badge bg="secondary"></Badge>
+      </h1>
+      <p>loreLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <img src={image} alt='robot'/>
+    <Button variant="dark"style={{margin:"50px"}} > Start</Button>
+    </div>
+    </Fragment>
+  );
+}
+
+export default Description
