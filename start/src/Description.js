aaf127d2ab08278afc4772065c011ac245e4e760
Shubham Patil <shubhampatil.ani@gmail.com>
2023-08-19 14:37:40 +0530

    2nd commit

--- start/src/Description.js
+++ start/src/Description.js
@@ -11,7 +11,7 @@ import Button from "react-bootstrap/esm/Button";
 const Description = () => {
       return (
     <Fragment className={classes.body}>
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className={classes.sticky}>
       <Container>
         <Navbar.Brand href="#home">Robot character</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -40,10 +40,10 @@ const Description = () => {
       </h1>
       <p>loreLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     <img src={image} alt='robot'/>
-    <Button variant="dark"style={{margin:"50px"}} > Start</Button>
+    <Button variant="dark">START</Button>
     </div>
     </Fragment>
   );
-}
+}  
 
 export default Description
