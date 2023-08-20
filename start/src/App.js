71fd087562cddffb937928dd0021f8e84da6b2c6
Shubham Patil <shubhampatil.ani@gmail.com>
2023-08-20 18:55:11 +0530

    added react router

--- start/src/App.js
+++ start/src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
 import { Fragment } from 'react';
+import { Route, Switch } from "react-router-dom";
 import Description from './Description';
+import PartsSelection from './PartsSelection';
 
 function App() {
   return (
     <Fragment>
-      <Description/>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Description/>
+          </Route>
+          <Route path="/home" >
+            <PartsSelection/>
+          </Route>
+        </Switch>
+      </main>
     </Fragment>
   );
 }
--- start/src/Description.js
+++ start/src/Description.js
@@ -7,13 +7,14 @@ import Badge from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from "react-bootstrap/esm/Button";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Description = () => {
       return (
     <Fragment className={classes.body}>
         <Navbar expand="lg" className={classes.sticky}>
       <Container>
-        <Navbar.Brand href="#home">Robot character</Navbar.Brand>
+        <Navbar.Brand href="#home">Robot character Description</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -40,7 +41,7 @@ const Description = () => {
       </h1>
       <p>loreLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     <img src={image} alt='robot'/>
-    <Button variant="dark">START</Button>
+    <Link to="/home"><Button variant="dark">START</Button></Link>
     </div>
     </Fragment>
   );
